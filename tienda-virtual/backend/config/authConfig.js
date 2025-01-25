5c4083150c51dcfbc28c23ce8365a1f3c7ae9ca0
Andres Gonzalez <147012730+Andrewgo12@users.noreply.github.com>
2025-01-25 15:41:19 -0500

    Delete tienda-virtual/backend directory

--- tienda-virtual/backend/config/authConfig.js
+++
@@ -1,5 +0,0 @@
-// config/authConfig.js
-module.exports = {
-    jwtExpiration: '1h', // Duración del token
-    jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret', // Clave secreta
-};
--- tienda-virtual/backend/config/serverConfig.js
+++
@@ -1,7 +0,0 @@
-// config/serverConfig.js
-module.exports = {
-    port: process.env.PORT || 5000,
-    jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret',
-    mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/tienda-virtual',
-  };
-  
